b7842508dd7e6f0ed58cb44809247107df1beda8
jinmlee <127607365+jinmlee@users.noreply.github.com>
2024-07-10 02:36:20 +0900

    feat(view): 글 조회 뷰 기능 구현

---
+++ src/main/java/me/jinmlee/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,23 @@
+package me.jinmlee.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jinmlee.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
