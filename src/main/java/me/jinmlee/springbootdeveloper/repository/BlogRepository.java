702ded9984b76861855edd603017eafcbfc52d0f
jinmlee <127607365+jinmlee@users.noreply.github.com>
2024-07-09 22:11:41 +0900

    feat: Article 저장 기능 구현

---
+++ src/main/java/me/jinmlee/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package me.jinmlee.springbootdeveloper.repository;
+
+import me.jinmlee.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
---
+++ src/main/java/me/jinmlee/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,18 @@
+package me.jinmlee.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.jinmlee.springbootdeveloper.domain.Article;
+import me.jinmlee.springbootdeveloper.dto.AddArticleRequest;
+import me.jinmlee.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+}
